1b65e5c376855c74f2ab752592b7c33a186adf6f
IgnacioMartinezInfante <145930821+IgnacioMartinezInfante@users.noreply.github.com>
2024-07-15 23:31:38 -0300

    game over, vida personaje y canvas, daño enemigos, contador botones

--- Assets/Scripts/Enemy_Damage.cs
+++ Assets/Scripts/Enemy_Damage.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class Enemy_Damage : MonoBehaviour
 {
     public int damage = 10; // Daño que inflige el enemigo al jugador
+    private bool canDamage = true; // Controla si el enemigo puede hacer daño
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Player_Health playerHealth = collision.gameObject.GetComponent<Player_Health>();
-            if (playerHealth != null)
+            if (playerHealth != null && canDamage)
             {
                 playerHealth.TakeDamage(damage);
+                StartCoroutine(DamageCooldown());
             }
         }
+    }
+
+    private IEnumerator DamageCooldown()
+    {
+        canDamage = false; // Desactiva el daño
+        yield return new WaitForSeconds(1f); // Espera 1 segundo
+        canDamage = true; // Activa el daño nuevamente
     }
 }
